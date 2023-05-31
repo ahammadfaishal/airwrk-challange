3da51871f548119112847140625b5002a940cf42
Faisal AL Mahmud <faisal@commengine.xyz>
2023-05-31 23:14:22 +0600

    initial commit

---
+++ AirWrkChallenge/Controllers/TextAnalyticsController.cs
@@ -0,0 +1,68 @@
+﻿using AirWrkChallenge.Models.TextAnalytics;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Hosting;
+using System.Text;
+
+namespace AirWrkChallenge.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class TextAnalyticsController : ControllerBase
+    {
+        [HttpPost]
+        [Route("analyze")]
+        public IActionResult Analyze(string text)
+        {
+            int characterCounter = 0;
+            int sentenceCounter = 0;
+            string mostFrequentWord = "";
+            int mostFrequentWordFrequency = 0;
+            string longestWord;
+
+            StringBuilder wordBuilder = new StringBuilder();
+            var words = new List<string>();
+            char previous;
+            foreach (char item in text)
+            {
+                if (!char.IsWhiteSpace(item))
+                {
+                    characterCounter++;
+
+                    if (item == '.')
+                    {
+                        sentenceCounter++;
+                        words.Add(wordBuilder.ToString().ToLower());
+                        wordBuilder.Clear();
+                    }
+                    else
+                    {
+                        wordBuilder.Append(item);
+                    }
+                }
+                else
+                {
+                    if (wordBuilder.Length > 0)
+                    {
+                        words.Add(wordBuilder.ToString().ToLower());
+                        wordBuilder.Clear();
+                    }
+                }
+            }
+
+            var wordGroups = words.GroupBy(x => x);
+
+            foreach (var wordGroup in wordGroups)
+            {
+                if (wordGroup.Count() > mostFrequentWordFrequency)
+                {
+                    mostFrequentWordFrequency = wordGroup.Count();
+                    mostFrequentWord = wordGroup.Key;
+                }
+            }
+
+            var output = new AnalyzeModel(characterCounter, words.Count, sentenceCounter, new MostFrequentWordDto(mostFrequentWord, mostFrequentWordFrequency), "");
+            return Ok(output);
+        }
+    }
+}
---
+++ AirWrkChallenge/Models/TextAnalytics/AnalyzeModel.cs
@@ -0,0 +1,49 @@
+﻿namespace AirWrkChallenge.Models.TextAnalytics
+{
+    public class AnalyzeModel
+    {
+        public AnalyzeModel()
+        {
+        }
+
+        public AnalyzeModel(int charCount, int wordCount, int sentenceCount, MostFrequentWordDto mostFrequentWord, string longestWord)
+        {
+            CharCount = charCount;
+            WordCount = wordCount;
+            SentenceCount = sentenceCount;
+            MostFrequentWord = mostFrequentWord;
+            LongestWord = new LongestWordDto(longestWord);
+        }
+
+        public int CharCount { get; set; }
+        public int WordCount { get; set; }
+        public int SentenceCount { get; set; }
+        public MostFrequentWordDto MostFrequentWord { get; set; }
+        public LongestWordDto LongestWord { get; set; }
+    }
+
+    public class MostFrequentWordDto
+    {
+        public MostFrequentWordDto(string word, int frequency)
+        {
+            Word = word;
+            Frequency = frequency;
+        }
+
+        public string Word { get; }
+        public int Frequency { get;  }
+    }
+
+    public class LongestWordDto
+    {
+        public LongestWordDto(string word)
+        {
+            Word = word;
+            Length = word.Length;
+        }
+
+        public string Word { get;  }
+        public int Length { get;  }
+    }
+
+}
